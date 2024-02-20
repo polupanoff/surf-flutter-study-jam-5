29024dfb1acd91d4e891b524af4ab661f470eb39
polupanoff <polupanoff@gmail.com>
2024-02-20 20:30:51 +0500

    update

--- meme_generator/lib/screen/meme_generator_screen.dart
+++ meme_generator/lib/screen/meme_generator_screen.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
-class MemeGeneratorScreen extends StatelessWidget {
+class MemeGeneratorScreen extends StatefulWidget {
   const MemeGeneratorScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MemeGeneratorScreen> createState() => _MemeGeneratorScreenState();
+}
+
+class _MemeGeneratorScreenState extends State<MemeGeneratorScreen> {
+  String textURL = "";
+  String textMem = "";
+  late TextEditingController controllerURL;
+  late TextEditingController controllerMem;
+
+  @override
+  void initState() {
+    super.initState();
+    controllerURL = TextEditingController();
+    controllerMem = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,25 +47,40 @@ class MemeGeneratorScreen extends StatelessWidget {
                   SizedBox(
                     width: double.infinity,
                     height: 200,
+                    child: InkWell(
+                      child: DecoratedBox(
+                        decoration: decoration,
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.network(
+                            textURL.isEmpty
+                                ? "https://i.cbc.ca/1.6713656.1679693029!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/this-is-fine.jpg"
+                                : textURL,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      onTap: () async {
+                        showInput();
+                      },
+                    ),
+                  ),
+                  InkWell(
                     child: DecoratedBox(
                       decoration: decoration,
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Image.network(
-                          'https://i.cbc.ca/1.6713656.1679693029!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/this-is-fine.jpg',
-                          fit: BoxFit.cover,
+                      child: Text(
+                        textMem.isEmpty ? "Здесь мог бы быть ваш мем" : textMem,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontFamily: 'Impact',
+                          fontSize: 40,
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                  ),
-                  const Text(
-                    'Здесь мог бы быть ваш мем',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Impact',
-                      fontSize: 40,
-                      color: Colors.white,
-                    ),
+                    onTap: () async {
+                      showInput();
+                    },
                   ),
                 ],
               ),
@@ -57,5 +89,63 @@ class MemeGeneratorScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> showInput() async {
+    controllerURL.text = textURL;
+    controllerMem.text = textMem;
+
+    var result = await showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          children: [
+            Column(
+              children: [
+                TextField(
+                  controller: controllerURL,
+                ),
+                TextField(
+                  controller: controllerMem,
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop({
+                          'URL': controllerURL.text,
+                          'Mem': controllerMem.text
+                        });
+                      },
+                      child: Text("ОК"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Отмена"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        );
+      },
+    );
+
+    if (result != null) {
+      setState(
+        () {
+          textURL = result['URL'].isEmpty
+              ? ""
+              : result['URL'];
+
+          textMem = result['Mem'].isEmpty
+              ? ""
+              : result['Mem'];
+        },
+      );
+    }
   }
 }
